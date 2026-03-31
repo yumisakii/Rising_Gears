cdb47c6bd9afe9db886d49e17bb85f732d3f4746
yumisakii <jonathan.devaulchier@orange.fr>
2026-03-31 17:26:21 +0200

    add raycast

---
+++ Source/Rising_Gear/MyCharacter.cpp
@@ -0,0 +1,71 @@
+#include "MyCharacter.h"
+
+#include "Camera/CameraComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "InputAction.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Construct the camera component
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(RootComponent);
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(0.f, 0.f, 64.f));
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Cast the default input component to the Enhanced Input Component
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Bind the action. ETriggerEvent::Started means it fires exactly when the button is pressed down.
+		EnhancedInputComponent->BindAction(ShootGrapplingHookAction, ETriggerEvent::Started, this, &AMyCharacter::ShootGrapplingHook);
+	}
+}
+
+void AMyCharacter::ShootGrapplingHook()
+{
+	FVector StartLocation = FirstPersonCameraComponent->GetComponentLocation();
+
+	FVector ForwardDirection = FirstPersonCameraComponent->GetForwardVector();
+
+	float MaxGrappleDistance = 5000.0f;
+	FVector EndLocation = StartLocation + (ForwardDirection * MaxGrappleDistance);
+
+	FHitResult HitResult;
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECC_Visibility
+	);
+
+	// Draw Raycas line for debugging purposes. Green if it hits something, red if it doesn't.  
+	FColor LineColor = bHit ? FColor::Green : FColor::Red;
+	DrawDebugLine(GetWorld(), StartLocation, EndLocation, LineColor, false, 2.0f, 0, 2.0f);
+}
+
---
+++ Source/Rising_Gear/Rising_Gear.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Rising_Gear.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rising_Gear, "Rising_Gear" );
